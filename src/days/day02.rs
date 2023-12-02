9089868455b4c0f286e84cd053483f2f57316e40
Jakob Nybo Nissen <jakobnybonissen@gmail.com>
2023-12-02 20:52:11 +0100

    Add day 2

---
+++ src/days/day02.rs
@@ -0,0 +1,86 @@
+// TODO: Redo parsing, maybe with some kind of crate?
+// solve_parsed can be more elegant
+
+struct Draw([u32; 3]);
+
+impl Draw {
+    fn empty() -> Self {
+        Draw([0; 3])
+    }
+}
+
+struct Game(Vec<Draw>);
+
+impl Game {
+    fn is_possible(&self, max: Draw) -> bool {
+        self.0.iter().all(|draw| {
+            draw.0.iter().zip(max.0.iter()).all(|(a, b)| a <= b)
+        })
+    }
+
+    fn max_drawn(&self) -> Draw {
+        let mut result = Draw::empty().0;
+        for d in self.0.iter() {
+            for i in 0..result.len() {
+                result[i] = result[i].max(d.0[i])
+            }
+        }
+        Draw(result)
+    }
+}
+
+const MAX_DRAW: Draw = Draw([12, 13, 14]);
+
+// TODO: Draw twice in one round - validate
+fn parse_draw(s: &str) -> Draw {
+    let mut result = Draw([0; 3]);
+    for cube in s.trim().split(", ") {
+        let (a, b) = cube.trim().split_once(' ').unwrap();
+        let n = a.parse::<u32>().unwrap();
+        match b {
+            "red" => {result.0[0] = n},
+            "green" => {result.0[1] = n},
+            "blue" => {result.0[2] = n},
+            _ => unreachable!(),
+        };
+    }
+    result
+}
+
+// TODO: Compile time regex
+fn parse_game(s: &str) -> Game {
+    let (_, rest) = s.split_once(": ").unwrap();
+    Game(rest.split("; ").map(parse_draw).collect())
+}
+
+fn parse(s: &str) -> Vec<Game> {
+    s.lines().map(str::trim).filter(|s| !s.is_empty()).map(parse_game).collect()
+}
+
+// TODO Slice of as ref slice?
+fn solve_parsed(v: &[Game]) -> (usize, usize) {
+    v.iter().enumerate().fold((0, 0), |(p1, p2), (i, game)| {
+        (
+            p1 + (i + 1) * (game.is_possible(MAX_DRAW) as usize),
+            p2 + game.max_drawn().0.iter().map(|i| *i as usize).product::<usize>()
+        )
+    })
+}
+
+pub fn solve(s: &str) -> (usize, usize) {
+    solve_parsed(&parse(s))
+}
+
+#[cfg(test)]
+mod tests {
+    static TEST_STR: &str = "Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
+    Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue
+    Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red
+    Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red
+    Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green";
+
+    #[test]
+    fn test() {
+        assert_eq!(super::solve(TEST_STR), (8, 2286));
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,33 @@
-// TODO:
-// Check for data dir's existence
+// TODO: If pick day not impl, do not error if file does not exist.
+// 1. Parse days
+// 2. Fetch functions for each day
+// 3. Fetch data for each day
+// 4. Compute and print
 
 mod days;
 
 use std::{
+    cell::OnceCell,
     fmt::Display,
     path::{Path, PathBuf},
+    time::Duration,
 };
 
 use clap::{self, Parser, Subcommand};
+use reqwest::blocking::Client;
 
-type Boxes = (Box<dyn Display>, Box<dyn Display>);
+type TimedBoxes = (Duration, Box<dyn Display>, Box<dyn Display>);
 
-fn to_boxed<F, A, B>(f: F, s: &str) -> Option<Boxes>
+fn to_boxed<F, A, B>(f: F, s: &str) -> Option<TimedBoxes>
 where
     F: Fn(&str) -> (A, B),
     A: Display + 'static,
     B: Display + 'static,
 {
+    let start = std::time::Instant::now();
     let (a, b) = f(s);
-    Some((Box::new(a), Box::new(b)))
+    let elapsed = start.elapsed();
+    Some((elapsed, Box::new(a), Box::new(b)))
 }
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord)]
@@ -44,6 +52,13 @@ impl Day {
     }
 }
 
+fn parse_days<T: AsRef<str>>(v: &[T]) -> Vec<Day> {
+    let mut days: Vec<_> = v.iter().map(|i| Day::from_str(i.as_ref())).collect();
+    days.sort_unstable();
+    days.dedup();
+    days
+}
+
 // TODO: Add proper errors
 fn load_day(path: &Path) -> String {
     std::fs::read_to_string(path).unwrap()
@@ -56,23 +71,111 @@ fn load_days(dir: &Path, days: &[Day]) -> Vec<(Day, String)> {
         .collect()
 }
 
-fn solve_days(days: &[(Day, &str)]) -> Vec<Option<Boxes>> {
+fn solve_days(days: &[(Day, &str)]) -> Vec<Option<TimedBoxes>> {
     days.iter()
         .map(|(day, s)| match day {
             Day(1) => to_boxed(days::day01::solve, s),
+            Day(2) => to_boxed(days::day02::solve, s),
             _ => None,
         })
         .collect()
 }
 
-fn print_solution(day: Day, solution: Option<Boxes>) {
-    println!("Day {:02}:", day.0);
-    if let Some((a, b)) = solution {
+fn print_solution(day: Day, solution: Option<TimedBoxes>) {
+    print!("Day {:02}", day.0);
+    if let Some((duration, a, b)) = solution {
+        println!(" [{:.2?}]:", duration);
         println!("  Part 1: {}", a);
         println!("  Part 2: {}\n", b);
     } else {
-        println!("  Unimplemented!\n")
+        println!(":\n  Unimplemented!\n")
     }
+}
+
+fn solve<T: AsRef<str>>(data_dir: &Path, day_strings: &[T]) {
+    if !data_dir.is_dir() {
+        eprint!(
+            "Data directory is not an existing file: \"{:#?}\"",
+            data_dir
+        );
+        std::process::exit(1)
+    }
+    // TODO: Print timings (parsing + solving)
+    let days = parse_days(day_strings);
+    let data: Vec<(Day, String)> = load_days(&data_dir, &days);
+    let solutions = solve_days(
+        &data
+            .iter()
+            .map(|(d, s)| (*d, s.as_str()))
+            .collect::<Vec<_>>(),
+    );
+    for (&day, solution) in days.iter().zip(solutions) {
+        print_solution(day, solution)
+    }
+}
+
+fn download(data_dir: &Path, day_strings: &[String]) {
+    // Make dir and verify it exists
+    if !data_dir.exists() {
+        if data_dir.parent().is_none() {
+            eprintln!(
+                "Data dir \"{:#?}\" does not exist, and cannot be created because it has no parent",
+                data_dir
+            );
+            std::process::exit(1)
+        }
+        std::fs::create_dir(data_dir).unwrap() // TODO: Error message
+    } else if !data_dir.is_dir() {
+        eprintln!(
+            "Data dir \"{:#?}\" exists, but is not a directory",
+            data_dir
+        );
+        std::process::exit(1)
+    }
+    let days = parse_days(&day_strings);
+    if days.is_empty() {
+        return;
+    }
+    let client_cell: OnceCell<Client> = OnceCell::new();
+    for day in days.iter() {
+        let path = data_dir.join(format!("day{:0>2}.txt", day.0));
+        if path.exists() {
+            println!("Input already exists: Day {:0>2}", day.0);
+        } else {
+            println!("Downloading day {:0>2}", day.0);
+            let client = client_cell.get_or_init(make_client);
+            let data = download_input(client, *day);
+            std::fs::write(path, data).unwrap() // TODO: Proper error
+        }
+    }
+}
+
+fn make_client() -> Client {
+    let mut headers = reqwest::header::HeaderMap::default();
+    let session = match std::env::var("ADVENTOFCODE_SESSION") {
+        Ok(s) => s,
+        Err(e) => {
+            println!(
+                "Error: Could not load environmental variable ADVENTOFCODE_SESSION: \"{}\"",
+                e
+            );
+            std::process::exit(1);
+        }
+    };
+    let cookie =
+        reqwest::header::HeaderValue::from_str(format!("session={}", session).as_str()).unwrap();
+    headers.insert("Cookie", cookie);
+    Client::builder().default_headers(headers).build().unwrap()
+}
+
+fn download_input(client: &Client, day: Day) -> String {
+    let url = format!("https://adventofcode.com/2023/day/{}/input", day.0);
+    let resp = client.get(url.as_str()).send().unwrap();
+    if !resp.status().is_success() {
+        eprintln!("Error when processing request:\n{}", resp.text().unwrap());
+        std::process::exit(1);
+    }
+    resp.text().unwrap()
 }
 
 #[derive(Subcommand)]
@@ -81,6 +184,10 @@ enum Commands {
         data_dir: PathBuf,
         day_strings: Vec<String>,
     },
+    Download {
+        data_dir: PathBuf,
+        day_strings: Vec<String>,
+    }, // TODO: Add benchmark (reading, parsing, solving)
 }
 
 #[derive(Parser)]
@@ -96,24 +203,10 @@ fn main() {
         Commands::Solve {
             data_dir,
             day_strings,
-        } => {
-            // TODO: Validate data dir's existence
-            let mut days: Vec<_> = day_strings
-                .iter()
-                .map(|i| Day::from_str(i.as_str()))
-                .collect();
-            days.sort_unstable();
-            days.dedup();
-            let data: Vec<(Day, String)> = load_days(&data_dir, &days);
-            let solutions = solve_days(
-                &data
-                    .iter()
-                    .map(|(d, s)| (*d, s.as_str()))
-                    .collect::<Vec<_>>(),
-            );
-            for (&day, solution) in days.iter().zip(solutions) {
-                print_solution(day, solution)
-            }
-        }
+        } => solve(&data_dir, &day_strings),
+        Commands::Download {
+            data_dir,
+            day_strings,
+        } => download(&data_dir, &day_strings),
     }
 }
